f795a01a54d642ed5a958fe29256941ea66ea920
magkuna <magkuna@gmail.com>
2019-03-18 11:40:50 +0100

    exercise 2

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-/*const fruit = [ "apple", "pear", "mango"];
-fruit.forEach((element,index,array) => { 
-    console.log(index)
-})
+/*function render(what, where, myArray) {
+			
+    var what = '<ol>';
 
-const fruit = [ "apple", "pear", "mango"];
-fruit.forEach((element,index,array) => { 
-    console.log(element)
-});*/
+    myArray.forEach(function(element, index, array) {
+        what += '<li>' + element + ' (' + index + ')</li>';
+    });
+    what += '</ol>';
 
-const fruit = [ "apple", "pear", "mango"];
-fruit.forEach((element,index,array) => { 
-    console.log(array);
-    console.log(index);
-    console.log(element);
+    where.innerHTML = what;
+    where.innerHTML += 'Zwrócony typ danych: <strong>' + typeof what + '</strong>';
+}
+
+var tekst = '';
+
+var body = document.body;
+// albo var body = document.getElementsByTagName('body')[0];
+var owoce = ['Kiwi', 'Mango', 'Ananas', 'Grapefruit'];
+render(tekst, body, owoce);*/
+
+const render = (what,where) =>{
+
+what += '<ol>'
+
+owoce.forEach ( (element, index, array) => { what += '<li>' + element + ' (' + index + ')</li>'
 });
 
+what += '</ol>'
+
+where.innerHTML = what
+where.innerHTML += 'Zwrócony typ danych: <strong>' + typeof what + '</strong>';
+}
+
+const owoce = ['Kiwi', 'Mango', 'Ananas', 'Grapefruit'];
+let html = ''
+let div = document.querySelector('div')
+render (html, div)
