849a91cdfb281183ef3efc19f5f006a31a766672
magkuna <magkuna@gmail.com>
2019-03-18 09:38:24 +0100

    sort, reverse

---
+++ script.js
@@ -0,0 +1,6 @@
+const continents = ["Europa","Azja", "Afryka"];
+
+console.log  (continents.sort());  // alfabetycznie
+console.log (continents.reverse()); // odwraca kolejnosc tablicy
+console.log (continents);
+
