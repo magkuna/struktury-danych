3a301577c1a0ac272ee33e2446f1bfef72339648
magkuna <magkuna@gmail.com>
2019-03-18 10:50:15 +0100

    for each wywołanie array index element

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const fruit = [ "apple", "pear", "mango"];
+/*const fruit = [ "apple", "pear", "mango"];
 fruit.forEach((element,index,array) => { 
     console.log(index)
+})
+
+const fruit = [ "apple", "pear", "mango"];
+fruit.forEach((element,index,array) => { 
+    console.log(element)
+});*/
+
+const fruit = [ "apple", "pear", "mango"];
+fruit.forEach((element,index,array) => { 
+    console.log(array);
+    console.log(index);
+    console.log(element);
 });
+
