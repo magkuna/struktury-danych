b90b7a91cead8b3590ef260f1014c5d8c7d92ceb
magkuna <magkuna@gmail.com>
2019-03-18 10:43:27 +0100

    exercise 1

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const continents = ["Europa","Azja", "Afryka"];
+// A
+/*const continents = ["Europa","Azja", "Antarktyda","Afryka", "Australia i Oceania", "Ameryka Północna", "Ameryka Południowa" ];
 
 console.log  (continents.sort());  // alfabetycznie
 console.log (continents.reverse()); // odwraca kolejnosc tablicy
-console.log (continents);
+console.log (continents);*/
 
+console.log ("napisane w render");
+
+const renderA = myArray =>{
+    console.log (myArray.sort());
+    console.log(myArray.reverse());
+};
+const kontynenty = ["Europa","Azja", "Antarktyda","Afryka", "Australia i Oceania", "Ameryka Północna", "Ameryka Południowa" ];
+renderA(kontynenty);
+
+
+
+// B
+const renderB = myArray => {
+console.log(myArray.sort((x, y) => x - y ));
+console.log(myArray.sort((x, y) => y - x )); 
+};
+
+const numbers = [1, 12, 13, 0, 2, 20, 9, 3, 22, 10, -2,11];
+renderB(numbers);
